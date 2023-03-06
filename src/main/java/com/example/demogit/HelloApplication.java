5a65a1be69f07daa04a4f04f66678002cfdfeec2
lucca <simone.lucca@mail.polimi.it>
2023-03-06 21:21:08 +0100

    a caso (la carta si muove adesso)

--- src/main/java/com/example/demogit/HelloApplication.java
+++ src/main/java/com/example/demogit/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Test comment
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("layoutProva.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("lobby.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Simone was here!");
         stage.setScene(scene);
